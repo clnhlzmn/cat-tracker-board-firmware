8c55969f4388b31007aa5bb9ef1dcdd97c47ab55
Colin Holzman <colin.holzman@protonmail.com>
2021-05-24 18:53:09 -0400

    Add Atmel Studio solution and project.

---
+++ firmware/cat-tracker-board-firmware/main.c
@@ -0,0 +1,21 @@
+/*
+ * cat-tracker-board-firmware.c
+ *
+ * Created: 5/24/2021 6:52:11 PM
+ * Author : Colin
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
