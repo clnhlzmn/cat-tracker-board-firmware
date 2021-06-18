834d91c37a7844f3dfe450e0786cd17d790f6b5f
Colin Holzman <colin.holzman@protonmail.com>
2021-06-18 09:38:14 -0400

    Add hello world printing from radio to main.cpp.

--- firmware/cat-tracker-board-firmware/main.cpp
+++ firmware/cat-tracker-board-firmware/main.cpp
@@ -39,17 +39,30 @@
 #include <stdio.h>
 #include "RH_RF95.h"
 
-RH_RF95 rf95;
+RH_RF95 rf95(SS_PIN, INTERRUPT_PIN);
+
+#define FREQUENCY 921.2
 
 int main(void)
 {
 	atmel_start_init();
+
+    bool radio_init = rf95.init();
+    if (!radio_init) {
+        printf("failed to initialize radio");
+        while (1) {}
+    }
+    rf95.setFrequency(FREQUENCY);
+    rf95.setTxPower(14, false);
     
     //set gps enable pin as output
     PORT->Group[0].DIRSET.reg = PORT_PA17;
 
 	while (1) {
     	printf("hello world\r\n");
+        const char *message = "hello world";
+        rf95.send((uint8_t*)message, strlen(message));
+        rf95.waitPacketSent();
     	delay_ms(1000);
         PORT->Group[0].OUTTGL.reg = PORT_PA17;
         cdc_device_acm_update();
