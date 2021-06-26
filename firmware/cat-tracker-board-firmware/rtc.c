e0c2de66ea148db2e17ff3fd566b7a88fdd82ca6
Colin Holzman <colin.holzman@protonmail.com>
2021-06-25 23:31:00 -0400

    Add checks in rtc for sync busy when setting timeout and change to use GCLKGEN3 which is already configured to use OSCULP32K by atmelt start.

--- firmware/cat-tracker-board-firmware/rtc.c
+++ firmware/cat-tracker-board-firmware/rtc.c
@@ -7,31 +7,43 @@
 
 #include "rtc.h"
 #include "sam.h"
+#include <stdlib.h>
+#include <stdbool.h>
+
+static bool rtc_is_syncing(void) {
+    return RTC->MODE0.STATUS.bit.SYNCBUSY;
+}
 
 void rtc_init(void) {
     PM->APBAMASK.bit.RTC_ = 1;
 
-    /*set GCLKGEN2 to use OSCULP32K with run in standby enabled*/
-    GCLK->GENCTRL.reg = GCLK_GENCTRL_SRC_OSCULP32K | GCLK_GENCTRL_RUNSTDBY | GCLK_GENCTRL_GENEN | GCLK_GENCTRL_ID(2);
+    /*set GCLKGEN3 to use OSCULP32K*/
+    /*GCLKGEN3 should already be set to use OSCULP32K by atmel start code*/
 
-    /*set RTC GCLK to use GCLKGEN2*/
-    GCLK->CLKCTRL.reg = GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK2 | GCLK_CLKCTRL_ID_RTC;
+    /*set RTC GCLK to use GCLKGEN3*/
+    GCLK->CLKCTRL.reg = GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK3 | GCLK_CLKCTRL_ID_RTC;
+    while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY) {}
 
     /*enable rtc in mode 0 with match clear*/
     RTC->MODE0.CTRL.reg = RTC_MODE0_CTRL_MODE(0) | RTC_MODE0_CTRL_ENABLE;
+    while (rtc_is_syncing()) {}
 
     NVIC_EnableIRQ(RTC_IRQn);
+    NVIC_SetPriority(RTC_IRQn, 0x00);
 }
 
 void rtc_set_timeout_ms(uint32_t ms) {
     RTC->MODE0.COMP[0].reg = ms * 33;
+    while (rtc_is_syncing()) {}
     RTC->MODE0.COUNT.reg = 0;
+    while (rtc_is_syncing()) {}
+    RTC->MODE0.INTFLAG.reg = RTC_MODE0_INTFLAG_CMP0;
     RTC->MODE0.INTENSET.reg = RTC_MODE0_INTENSET_CMP0;
 }
 
 void RTC_Handler(void) {
     if (RTC->MODE0.INTFLAG.bit.CMP0) {
         RTC->MODE0.INTFLAG.reg = RTC_MODE0_INTFLAG_CMP0;
-        RTC->MODE0.INTENCLR.reg = RTC_MODE0_INTENSET_CMP0;
+        RTC->MODE0.INTENCLR.reg = RTC_MODE0_INTENCLR_CMP0;
     }
 }
